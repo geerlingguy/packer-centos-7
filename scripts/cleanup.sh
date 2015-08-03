ba6a1c8f2c21e394b130b4ffd8a0f2e1e175fa51
Jeff Geerling <geerlingguy@mac.com>
2015-08-02 22:54:31 -0500

    Remove Ansible after box build completes, increase disk_size to 80 GB.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
+# Remove Ansible.
+yum -y remove ansible
+
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
