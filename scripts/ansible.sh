72f6040bf40a77a93ff18e59944c6aa06be7cc69
Jeff Geerling <geerlingguy@mac.com>
2016-03-13 15:38:02 -0500

    Use Ansible 2.0 when building the box.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,4 +4,7 @@
 rpm -ivh http://mirror.pnl.gov/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 
 # Install Ansible.
-yum -y install ansible python-setuptools
+yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel
+yum clean all
+easy_install pip
+pip install ansible
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eux
 
-# Remove Ansible.
-yum -y remove ansible
+# Remove Ansible and its dependencies.
+pip freeze | grep -v "^-e" | xargs pip uninstall -y
+yum -y remove git python-setuptools gcc libffi-devel python-devel openssl-devel
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
