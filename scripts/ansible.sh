84b05858bfa421ea03f312dc0ee880b1f1aaa2fb
PeterMika <peter.c.mika@gmail.com>
2016-05-17 16:14:07 -0400

    Change is intended to resolve packer provisioning error "virtualbox-iso: ERROR! Unexpected Exception: (setuptools 0.9.8 (/usr/lib/python2.7/site-packages), Requirement.parse('setuptools>=11.3'))
    I resolve issue by not installng python setuptools centos package but instead installing setuptools from source globally. see https://pypi.python.org/pypi/setuptools#unix-wget.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,7 +4,8 @@
 rpm -ivh http://mirror.pnl.gov/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 
 # Install Ansible.
-yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel
+yum -y install git gcc sudo libffi-devel python-devel openssl-devel wget
 yum clean all
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 easy_install pip
 pip install ansible
