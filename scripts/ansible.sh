d8abfa429029cba2fd6abceaf03e166481b2b9cf
Jeff Geerling <geerlingguy@mac.com>
2016-05-27 12:21:34 -0500

    Update ansible script for simpler installation.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -eux
 
 # Install EPEL repository.
-rpm -ivh http://mirror.pnl.gov/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum -y install epel-release
 
 # Install Ansible.
-yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel
-yum clean all
-easy_install pip
-pip install ansible
+yum -y install ansible
