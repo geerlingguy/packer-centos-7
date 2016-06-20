8a24d9bcdb5126d50a6d1ded22b447bd4297fc13
PeterMika <peter.c.mika@gmail.com>
2016-06-20 16:59:12 -0400

    This commit introduces an aws build option.

---
+++ scripts/create-user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+# Script is necessary when using AWS builder. Creates vagrant user. Virtualbox and VMWare use a kickstart file to do this.
+
+sudo useradd -m -s /bin/bash -G wheel vagrant
+echo "vagrant" | sudo  passwd --stdin vagrant
+
+mkdir /home/vagrant/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" >> /home/vagrant/.ssh/authorized_keys
