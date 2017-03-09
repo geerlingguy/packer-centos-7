22e73e03ef5c593da2ef7ee46db6b94e8156ab63
François Garillot <francois@garillot.net>
2017-03-09 11:20:53 -0500

    Packer finally concludes !!

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,7 +4,7 @@
 yum -y remove ansible
 
 # Zero out the rest of the free space using dd, then delete the written file.
-dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/EMPTY bs=1M || true
 rm -f /EMPTY
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
